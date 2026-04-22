47abbae07280a5f5bd2bf4c7fe07f76112e3885e
soekhant01 <soekhant.dev@gmail.com>
2026-04-22 22:39:31 +0630

    Login and SignUp Screen implemented

--- app/src/main/java/com/droid/letsbuy/screen/LoginScreen.kt
+++ app/src/main/java/com/droid/letsbuy/screen/LoginScreen.kt
@@ -1,10 +1,113 @@
 package com.droid.letsbuy.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.droid.letsbuy.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
-    Text("Login")
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome Back!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp, fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+            )
+        )
+        Spacer(Modifier.height(10.dp))
+
+
+        Text(
+            text = "Please Login!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp,
+            )
+        )
+        Spacer(Modifier.height(20.dp))
+
+
+        Image(
+            painter = painterResource(R.drawable.login),
+            contentDescription = "signup"
+        )
+
+
+
+
+        Spacer(Modifier.height(10.dp))
+
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(10.dp))
+
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text("Password")
+
+            },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+        Button(
+            onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Text("Login", fontSize = 22.sp)
+        }
+    }
 }
--- app/src/main/java/com/droid/letsbuy/screen/SignupScreen.kt
+++ app/src/main/java/com/droid/letsbuy/screen/SignupScreen.kt
@@ -1,11 +1,128 @@
 package com.droid.letsbuy.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.droid.letsbuy.R
 
 @Composable
 fun SignupScreen(modifier: Modifier = Modifier) {
-    Text("Signup")
+    var email by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello There!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp, fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+            )
+        )
+        Spacer(Modifier.height(4.dp))
+
+
+        Text(
+            text = "Create An Account!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp,
+            )
+        )
+
+
+
+        Image(
+            painter = painterResource(R.drawable.signup),
+            contentDescription = "signup",
+        )
+
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            label = {
+                Text("Name")
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(10.dp))
+
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            label = {
+                Text("Email")
+
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(Modifier.height(10.dp))
+
+
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            label = {
+                Text("Password")
+
+            },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(Modifier.height(20.dp))
+
+
+        Button(
+            onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+        ) {
+            Text("Signup", fontSize = 22.sp)
+        }
+    }
 
 }
