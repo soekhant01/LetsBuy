7e0510b9f371a91ced2cdaab4b8ffa983d68d091
soekhant01 <soekhant.dev@gmail.com>
2026-04-29 19:56:30 +0630

    designed product item and two item in a row

---
+++ app/src/main/java/com/droid/letsbuy/components/ProductItemView.kt
@@ -0,0 +1,91 @@
+package com.droid.letsbuy.components
+
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.droid.letsbuy.model.ProductModel
+
+@Composable
+fun ProductItemView(modifier: Modifier = Modifier, product: ProductModel) {
+
+    Card(
+        modifier = modifier.padding(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(4.dp)
+
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            AsyncImage(
+                model = product.images.firstOrNull(),
+                contentDescription = product.category,
+                modifier = Modifier
+                    .height(120.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                product.title,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "$${product.price}",
+                    fontSize = 14.sp,
+                    style = TextStyle(textDecoration = TextDecoration.LineThrough)
+                )
+                Spacer(Modifier.width(9.dp))
+                Text(
+                    text = "$${product.actualPrice}",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+
+                    )
+                Spacer(Modifier.weight(1f))
+
+                IconButton(onClick = {}) {
+                    Icon(
+                        Icons.Default.ShoppingCart,
+                        contentDescription = "Add to cart"
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/droid/letsbuy/pages/CategoryProductsPage.kt
+++ app/src/main/java/com/droid/letsbuy/pages/CategoryProductsPage.kt
@@ -1,23 +1,18 @@
 package com.droid.letsbuy.pages
 
-import android.util.Log
-import android.widget.Space
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.droid.letsbuy.components.ProductItemView
 import com.droid.letsbuy.model.ProductModel
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -50,11 +45,17 @@ fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId: String?) {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        items(productList.value) { item ->
-            Text("${item.title} ----> ${item.price}")
-            Spacer(Modifier.height(8.dp))
-            Text("${item.description}")
-            Spacer(Modifier.height(16.dp))
+        items(productList.value.chunked(2)) { rowItems ->
+
+            Row() {
+                rowItems.forEach { item ->
+                    ProductItemView(modifier = Modifier.weight(1f), item)
+                }
+                if (rowItems.size == 1) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+
 
         }
 
