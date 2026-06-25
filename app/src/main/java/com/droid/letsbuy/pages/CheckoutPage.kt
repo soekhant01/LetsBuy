7b1183c0dcc2fd354ea17b8ab5244fc795056766
soekhant01 <soekhant.dev@gmail.com>
2026-06-25 19:32:47 +0630

    top bar added in check out page

--- app/src/main/java/com/droid/letsbuy/pages/CheckoutPage.kt
+++ app/src/main/java/com/droid/letsbuy/pages/CheckoutPage.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,6 +23,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.droid.letsbuy.components.TopBar
 import com.droid.letsbuy.viewmodel.CheckOutViewModel
 
 @Composable
@@ -33,44 +37,55 @@ fun CheckoutPage(
     val tax by viewModel.tax.collectAsState()
     val total by viewModel.total.collectAsState()
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(horizontal = 16.dp)
-    ) {
-        Text("Checkout", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-        Spacer(Modifier.height(16.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("Deliver to : ", fontWeight = FontWeight.SemiBold)
-            Text(userModel.address)
+    Scaffold(
+        topBar = {
+            TopBar("")
         }
-        Spacer(Modifier.height(16.dp))
-
-        HorizontalDivider()
-        Spacer(Modifier.height(16.dp))
-        RowCheckout("Subtotal", subTotal)
-        Spacer(Modifier.height(8.dp))
-        RowCheckout("Discount", discount)
-        Spacer(Modifier.height(8.dp))
-        RowCheckout("Tax", tax)
-        Spacer(Modifier.height(16.dp))
-        HorizontalDivider()
-        Spacer(Modifier.height(16.dp))
-
-        RowCheckout("Total", total)
-
-        Spacer(Modifier.height(16.dp))
-
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            onClick = {}
+    ) { paddingValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(
+                    top = paddingValues.calculateTopPadding() - 16.dp,
+                    start = 16.dp,
+                    end = 16.dp
+                )
+                .verticalScroll(rememberScrollState()),
         ) {
-            Text("Pay Now")
+            Text("Checkout", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+            Spacer(Modifier.height(16.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Deliver to : ", fontWeight = FontWeight.SemiBold)
+                Text(userModel.address)
+            }
+            Spacer(Modifier.height(16.dp))
+
+            HorizontalDivider()
+            Spacer(Modifier.height(16.dp))
+            RowCheckout("Subtotal", subTotal)
+            Spacer(Modifier.height(8.dp))
+            RowCheckout("Discount", discount)
+            Spacer(Modifier.height(8.dp))
+            RowCheckout("Tax", tax)
+            Spacer(Modifier.height(16.dp))
+            HorizontalDivider()
+            Spacer(Modifier.height(16.dp))
+
+            RowCheckout("Total", total)
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                onClick = {}
+            ) {
+                Text("Pay Now")
+            }
         }
     }
 }
